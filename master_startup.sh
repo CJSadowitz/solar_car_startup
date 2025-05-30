47dc42ee3fa5fa86fd4697699e96a39b9a433281
colin sadowitz <sadowitzc@gmail.com>
2025-05-30 16:27:13 -0400

    Added db startup

--- master_startup.sh
+++ master_startup.sh
@@ -1,6 +1,7 @@
 echo "Master Script"
 date
 echo "Calling Child Scripts"
+bash child_scripts/database.sh
 bash child_scripts/can.sh &
 bash child_scripts/lte.sh &
 bash child_scripts/cloudflare.sh &
