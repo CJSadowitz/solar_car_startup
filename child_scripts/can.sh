9b5b70450092b2c55f26760885c1e77b20ae397e
colin sadowitz <sadowitzc@gmail.com>
2025-05-30 14:17:58 -0400

    finished_can_script

--- child_scripts/can.sh
+++ child_scripts/can.sh
@@ -1,6 +1,5 @@
 source child_scripts/can_util.sh
-trap "echo 'Stopping CAN setup'; exit" SIGINT
 echo "Attempting to initialize can network"
 set_up_can
-
-
+source ../can/venv/bin/activate
+python3 ../can/main.py
